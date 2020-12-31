296d7a3f2bbb34bea1ed5316c8c8e22a4a4bda5b
milan978gh <amilandev78@gmail.com>
2020-12-31 15:18:45 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,18 @@
+const questionPool = [
+  {
+    id: 1,
+    title: 'do you accept all major credit cards?',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est dolore illo dolores quia nemo doloribus quaerat, magni numquam repellat reprehenderit.'
+  },
+  {
+    id: 2,
+    title: 'do you support local farmers?',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est dolore illo dolores quia nemo doloribus quaerat, magni numquam repellat reprehenderit.'
+  },
+  {
+    id: 3,
+    title: 'do you support local farmers?',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est dolore illo dolores quia nemo doloribus quaerat, magni numquam repellat reprehenderit.'
+  }
+];
+
