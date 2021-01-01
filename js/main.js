5dab22841c17f97757c0b0fb5aabec80ef07eae5
milan978gh <amilandev78@gmail.com>
2021-01-01 12:24:56 +0100

    js done

--- js/main.js
+++ js/main.js
@@ -11,8 +11,51 @@ const questionPool = [
   },
   {
     id: 3,
-    title: 'do you support local farmers?',
+    title: 'do you use organic ingredients?',
     text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est dolore illo dolores quia nemo doloribus quaerat, magni numquam repellat reprehenderit.'
   }
 ];
+const sectionCenter = document.querySelector('.section-center');
 
+
+window.addEventListener('DOMContentLoaded', () => {
+  displayQuestions(questionPool);
+});
+
+
+function displayQuestions(quesItems) {
+  let queArticle = quesItems.map((item) => {
+    return `<article class="question">
+      <div class="question-title">
+        <p>${item.title}</p>
+        <button type="button" class="question-btn">
+          <img src="./img/plus-math-26.png">
+        </button>
+      </div>
+      <div class="question-text">
+        <p>${item.text}</p>
+      </div>
+    </article>`;
+  });
+  queArticle = queArticle.join('');
+  sectionCenter.innerHTML = queArticle;
+
+  buttonOperator();
+}
+
+function buttonOperator() {
+  const questions = sectionCenter.querySelectorAll('.question');
+
+  questions.forEach((question) => {
+    const btn = question.querySelector('.question-btn');
+    
+    btn.addEventListener('click', () => {
+      questions.forEach((item) => {
+        if (item !== question) {
+          item.classList.remove('show-text');
+        }
+      });
+      question.classList.toggle('show-text');
+    });
+  });
+}
